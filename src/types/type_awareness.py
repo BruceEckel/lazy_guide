152515cb61c1dc577e23acfe8fca0e0379e6d06e
Bruce Eckel <mindviewinc@gmail.com>
2024-11-12 16:49:54 -0700

    Just putting validate_output.py in every subdirectory (!)

---
+++ src/types/type_awareness.py
@@ -0,0 +1,38 @@
+#: type_awareness.py
+from validate_output import console
+
+x = True
+print(f"{type(True) = }")
+console == """
+type(True) = <class 'bool'>
+"""
+
+x = "a string"
+print(f"{type(x) = }")
+console == """
+type(x) = <class 'str'>
+"""
+
+x = None
+print(f"{type(x) = }")
+console == """
+type(x) = <class 'NoneType'>
+"""
+
+x = 1.23
+print(f"{type(x) = }")
+console == """
+type(x) = <class 'float'>
+"""
+
+x = 11
+print(f"{type(x) = }")
+console == """
+type(x) = <class 'int'>
+"""
+
+x = {"a", "b", "c"}
+print(f"{type(x) = }")
+console == """
+type(x) = <class 'set'>
+"""

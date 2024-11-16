269cc0b89e176ea3dd90fb7b5bbe0cdd94398747
Bruce Eckel <mindviewinc@gmail.com>
2024-11-16 15:56:05 -0700

    Create design_by_contract.py

---
+++ src/types/design_by_contract.py
@@ -0,0 +1,43 @@
+#: design_by_contract.py
+from typing import Final
+
+from validate_output import console
+
+
+def invariant(balance: float) -> float:
+    assert balance >= 0
+    return balance
+
+
+def deposit(balance: float, amount: float) -> float:
+    invariant(balance)
+    # Precondition
+    assert amount > 0
+    old_balance: Final[float] = balance
+    new_balance: Final[float] = old_balance + amount
+    # Postcondition
+    assert new_balance > old_balance
+    return invariant(new_balance)
+
+
+def withdraw(balance: float, amount: float) -> float:
+    invariant(balance)
+    # Preconditions
+    assert balance >= amount
+    assert amount > 0
+    old_balance: Final[float] = balance
+    new_balance: Final[float] = old_balance - amount
+    # Postcondition
+    assert new_balance < old_balance
+    return invariant(new_balance)
+
+
+print(balance := deposit(100.0, 50.0))
+console == """
+150.0
+"""
+
+print(withdraw(balance, 30.0))
+console == """
+120.0
+"""

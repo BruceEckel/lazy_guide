cb6827576992e6bf03e6fa8d7064a273204a0f93
Bruce Eckel <BruceTEckel@gmail.com>
2024-12-12 18:24:35 -0700

    Update reveal_type.py

--- src/types/reveal_type.py
+++ src/types/reveal_type.py
@@ -9,9 +9,9 @@ print(f"{reveal_type(s) = }")
 #| reveal_type(s) = 'a string'
 
 n = None
-print(f"{reveal_type(n)}")
+print(f"{reveal_type(n) = }")
 #| reveal_type(n) = None
 
 n = 1.23
-print(f"{reveal_type(n)}")
+print(f"{reveal_type(n) = }")
 #| reveal_type(n) = 1.23

036234d6a3849697550fcdb1476894994ba29047
Bruce Eckel <BruceTEckel@gmail.com>
2024-12-12 18:23:08 -0700

    Update reveal_type.py

---
+++ src/types/reveal_type.py
@@ -0,0 +1,17 @@
+#: reveal_type.py
+from typing import reveal_type
+
+b: bool = True
+print(f"{reveal_type(b) = }")
+
+s = "a string"
+print(f"{reveal_type(s) = }")
+#| reveal_type(s) = 'a string'
+
+n = None
+print(f"{reveal_type(n)}")
+#| reveal_type(n) = None
+
+n = 1.23
+print(f"{reveal_type(n)}")
+#| reveal_type(n) = 1.23
